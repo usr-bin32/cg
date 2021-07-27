81f836dae328626722f7f77f0a580e76f3a70b31
Gabriel <71942269+usr-bin32@users.noreply.github.com>
2021-07-27 18:49:17 -0300

    fix shadow

--- works/engine/states/SimulationState.js
+++ works/engine/states/SimulationState.js
@@ -8,6 +8,7 @@ import { PhysicsSystem } from "../systems/PhysicsSystem.js";
 import { SimulationAircraft } from "../entities/SimulationAircraft.js";
 import { CameraToggleSystem } from "../systems/CameraToggleSystem.js";
 import { GLTFLoader } from "../../../build/jsm/loaders/GLTFLoader.js";
+import { createGroundPlane } from "../../../libs/util/util.js";
 
 const SCALE = 500;
 
@@ -42,7 +43,10 @@ export const SimulationState = {
       const loader = new GLTFLoader();
       loader.load("engine/assets/mountains.glb", (gltf) => {
         gltf.scene.scale.set(SCALE, SCALE, SCALE);
-        gltf.scene.receiveShadow = true;
+        gltf.scene.traverse((child) => {
+          child.receiveShadow = true;
+        });
+
         scene.add(gltf.scene);
       });
     })();
@@ -69,7 +73,7 @@ export const SimulationState = {
 function addLighting(
   scene,
   target,
-  position = new THREE.Vector3(0, 100000, 125000)
+  position = new THREE.Vector3(0, 50000, 75000)
 ) {
   const ambientLight = new THREE.HemisphereLight("white", "darkslategrey", 0.5);
 
@@ -77,8 +81,8 @@ function addLighting(
   sunLight.position.copy(position);
   sunLight.target = target;
   sunLight.castShadow = true;
-  sunLight.shadow.mapSize.width = 2048 * 2;
-  sunLight.shadow.mapSize.height = 2048 * 2;
+  sunLight.shadow.mapSize.width = 2048 * 4;
+  sunLight.shadow.mapSize.height = 2048 * 4;
 
   sunLight.shadow.camera.visible = true;
   sunLight.shadow.camera.left = -100;
@@ -86,7 +90,7 @@ function addLighting(
   sunLight.shadow.camera.top = 100;
   sunLight.shadow.camera.bottom = -100;
   sunLight.shadow.camera.near = 0.1;
-  sunLight.shadow.camera.far = 50000;
+  sunLight.shadow.camera.far = 500000;
 
   scene.add(ambientLight);
   scene.add(sunLight);
